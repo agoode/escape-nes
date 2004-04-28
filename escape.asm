dbc649bc4672fb0b0b69de903b69aa80a0abb246
Adam Goode <adam@spicenitz.org>
2004-04-28 05:13:25 +0000

    working ok, splitting up stuff

--- escape.asm
+++ escape.asm
@@ -58,14 +58,22 @@ author:	.ds	20
 	
 ;;; initialize
 	.code
-	.bank	1
+	.bank	0
 	.org	$8000
+		
+	.include "rle.asm"			
+	.include "joystick.asm"	
+	.include "guy.asm"	
+	.include "load-draw-level.asm"
 	
+
+	.bank	1
+	.org	$A000
 
 		
 	.bank	2
 	.org	$C000
-	
+		
 	.include "interrupts.asm"
 
 		
@@ -111,11 +119,13 @@ start2:
 ppu_on:	
 	debug_p ds_ppu
 	jsr	vwait
-	lda	#%00000000
- 	sta	$2000
-	lda	#%00011010
+	lda	#%10000000
+ 	sta	$2000	
+	jsr	vwait
+	lda	#%00011110
  	sta	$2001
-
+	
+	
 main_loop:
 	jsr	handle_joy
 	jsr	vwait
@@ -188,106 +198,14 @@ choose_level:
 
 	rts
 	
-	
-handle_joy:
-	;; joystick
-	lda	#1
-	sta	$4016
-	lda	#0
-	sta	$4016
-
-	lda	$4016
-	lda	$4016
-	lda	$4016
-	
-	lda	$4016
-	and	#%00000001	; start
-	beq	.j_up
-	inc	level_num
-	lda	level_num
-	cmp	#12
-	bne	.continue
-	lda	#0
-	sta	level_num
-	
-.continue
-	jmp	start2
-	
-.j_up:	
-	lda	$4016
-	and	#%00000001
-	beq	.j_down		; up
- 	mov	#dir_up, gd
-	jsr	draw_guy
-
-.j_down:
-	lda	$4016
-	and	#%00000001
-	beq	.j_left		; down
- 	mov	#dir_down, gd
-	jsr	draw_guy
-
-.j_left:
-	lda	$4016
-	and	#%00000001
-	beq	.j_right
-  	mov	#dir_left, gd
- 	jsr	draw_guy
-
-				; left
-.j_right:
-	lda	$4016
-	and	#%00000001
-	beq	.done
- 	mov	#dir_right, gd
-	jsr	draw_guy
-				; right
-.done:	
-	rts
 
 	
 vwait:	
 	lda	$2002
 	bpl	vwait
 
-	lda	#0
-	sta	$2006
-	sta	$2006
-	lda	x_scroll
-	sta	$2005
-	lda	#0
-	sta	$2005
-
-	lda	#sprite/$100	; sprite
-	sta	$4014
 	rts
 
-
-draw_level:
-	;; assumes load_level just called
-	mov	#0, tile_pos
-.loop:	lda	tile_pos
-	cmp	#180
-	beq	.done
-	bit	#%00000100
-	bne	.continue
-	jsr	vwait
-.continue:	
-	ldx	tile_pos
-
-	;; get the tile
-	lda	tiles, X
-	tax
-	lda	tile_name_table, X
-	sta	tile
-	lda	tile_attr_table, X
-	sta	tile+1	
-
-	jsr	draw_tile
-	inc	tile_pos
-	jmp	.loop
-		
-.done:	rts
 
 
 init_sprite_memory:
@@ -299,406 +217,6 @@ init_sprite_memory:
 
 
 	
-draw_guy:
-	debug_p	ds_draw_guy
-	lda	gx
-	asl	a
-	asl	a
-	asl	a
-	asl	a
-	sec
- 	sbc	x_scroll
-	tax
-	
-	lda	gy
-	asl	a
-	asl	a
-	asl	a
-	asl	a
-	clc
-	adc	#$2F
-	tay	
-
-	lda	gd
-	sta	debug_num
-	cmp	#dir_up
-	beq	.up
-
-.compare2:	
-	cmp	#dir_down
-	beq	.down_trampoline
-	cmp	#dir_right
-	beq	.right_trampoline
-	cmp	#dir_left
-	beq	.left_trampoline
-
-.left_trampoline:
-	jmp	.left	
-.down_trampoline:
-	jmp	.down
-.right_trampoline:
-	jmp	.right
-
-.up:
-	sty	sprite
-	lda	guy_u_tile
-	sta	sprite+1
-	lda	#0
-	sta	sprite+2
-	stx	sprite+3
-
-	sty	sprite+4
-	lda	guy_u_tile+1
-	sta	sprite+5
-	lda	#0
-	sta	sprite+6
-	txa
-	clc
-	adc	#$8
-	sta	sprite+7
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+8
-	lda	guy_u_tile+2
-	sta	sprite+9
-	lda	#0
-	sta	sprite+10
-	stx	sprite+11
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+12
-	lda	guy_u_tile+3
-	sta	sprite+13
-	lda	#0
-	sta	sprite+14
-	txa
-	clc
-	adc	#$8
-	sta	sprite+15
-
-	jmp	.done
-.down:	
-	sty	sprite
-	lda	guy_d_tile
-	sta	sprite+1
-	lda	#0
-	sta	sprite+2
-	stx	sprite+3
-
-	sty	sprite+4
-	lda	guy_d_tile+1
-	sta	sprite+5
-	lda	#0
-	sta	sprite+6
-	txa
-	clc
-	adc	#$8
-	sta	sprite+7
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+8
-	lda	guy_d_tile+2
-	sta	sprite+9
-	lda	#0
-	sta	sprite+10
-	stx	sprite+11
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+12
-	lda	guy_d_tile+3
-	sta	sprite+13
-	lda	#0
-	sta	sprite+14
-	txa
-	clc
-	adc	#$8
-	sta	sprite+15
-
-	jmp	.done
-
-.left:	
-	sty	sprite
-	lda	guy_l_tile
-	sta	sprite+1
-	lda	#0
-	sta	sprite+2
-	stx	sprite+3
-
-	sty	sprite+4
-	lda	guy_l_tile+1
-	sta	sprite+5
-	lda	#0
-	sta	sprite+6
-	txa
-	clc
-	adc	#$8
-	sta	sprite+7
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+8
-	lda	guy_l_tile+2
-	sta	sprite+9
-	lda	#0
-	sta	sprite+10
-	stx	sprite+11
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+12
-	lda	guy_l_tile+3
-	sta	sprite+13
-	lda	#0
-	sta	sprite+14
-	txa
-	clc
-	adc	#$8
-	sta	sprite+15
-
-	jmp	.done
-	
-.right:
-	sty	sprite
-	lda	guy_l_tile+1
-	sta	sprite+1
-	lda	#%01000000
-	sta	sprite+2
-	stx	sprite+3
-
-	sty	sprite+4
-	lda	guy_l_tile
-	sta	sprite+5
-	lda	#%01000000
-	sta	sprite+6
-	txa
-	clc
-	adc	#$8
-	sta	sprite+7
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+8
-	lda	guy_l_tile+3
-	sta	sprite+9
-	lda	#%01000000
-	sta	sprite+10
-	stx	sprite+11
-
-	tya
-	clc
-	adc	#$8
-	sta	sprite+12
-	lda	guy_l_tile+2
-	sta	sprite+13
-	lda	#%01000000
-	sta	sprite+14
-	txa
-	clc
-	adc	#$8
-	sta	sprite+15
-
-	jmp	.done
-
-.done:	
-	rts
-
-	
-draw_tile:
-screen_pos .equ	tmp16
-	ldx	tile_pos
-	mov	tile_pos_table_2, X, screen_pos+1
-	mov	tile_pos_table_1, X, screen_pos
-
-	add16	screen_pos, #$20C0
-
-	lda	screen_pos
-	asl	a
-	lda	screen_pos+1
-	rol	a
-	and	#%00000111
-	asl	a
-	asl	a
-	asl	a
-	sta	tmp16_2+1
-	lda	screen_pos
-	and	#%00011100
-	lsr	a
-	lsr	a
-	ora	tmp16_2+1
-	adc	#$C0	
-
-	sta	tmp
-	lda	screen_pos+1
-	and	#%11111100
-	clc
-	adc	#$3	
-	sta	$2006		; set address of thing
-	tay
-	lda	tmp
-	sta	$2006	
-	ldx	$2007		; invalid data
-	ldx	$2007		; correct data
-	sty	$2006		; reset address
-	sta	$2006
-
-	lda	screen_pos	; find the bit
-	and	#%01000010
-	bne	.test1
-	lda	tile+1
-	and	#%00000011
-	sta	tmp
-	txa
-	ora	tmp
-	sta	$2007		; set the color
-	jmp	.update_tile
-
-.test1:	cmp	#%00000010
-	bne	.test2
-	lda	tile+1
-	and	#%00001100
-	sta	tmp
-	txa
-	ora	tmp
-	sta	$2007
-	jmp	.update_tile
-
-.test2:	cmp	#%01000000
-	bne	.test3
-	lda	tile+1
-	and	#%00110000
-	sta	tmp
-	txa
-	ora	tmp
-	sta	$2007
-	jmp	.update_tile
-
-.test3:	lda	tile+1
-	and	#%11000000
-	sta	tmp
-	txa
-	ora	tmp
-	sta	$2007
-	
-
-.update_tile:						
-	lda	tmp16+1
-	sta	$2006
-	lda	tmp16
-	sta	$2006
-	lda	tile
-	sta	$2007		; update the tile
-
-	add16	tmp16, #1	; next part (right)
-	lda	tmp16+1
-	sta	$2006
-	lda	tmp16
-	sta	$2006
-	inc	tile
-	lda	tile
-	sta	$2007		; update the tile
-
-	add16	tmp16, #$20	; next part (down)
-	lda	tmp16+1
-	sta	$2006
-	lda	tmp16
-	sta	$2006
-	lda	tile
-	clc
-	adc	#$10
-	sta	tile
-	sta	$2007		; update the tile
-
-	add16	tmp16, #$FFFF	; last part (left)
-	lda	tmp16+1
-	sta	$2006
-	lda	tmp16
-	sta	$2006
-	dec	tile
-	lda	tile
-	sta	$2007		; update the tile
-
-	rts
-
-
-
-load_level:
-	mov	#0, tmp_size+1
-	mov	#dir_right, gd
-	mov	#0, x_scroll
-			
-	mov16	level_addr, idx16
-	ldy	#0
-	mov	[idx16], Y, tmp
-	iny
-	mov	[idx16], Y, tmp
-	iny
-	mov	[idx16], Y, tmp
-	iny
-	mov	[idx16], Y, tmp
-	ldy	#15		; ESXL + width + height + MSB of size of title
-	mov	[idx16], Y, tmp_size	; size of title string
-	add16	idx16, #16		; move to start of title
-
-	;; copy the title string
-	strcpyp2c	idx16, title, tmp_size
-
-	add16	idx16, tmp_size	; move past the title string
-	ldy	#3		; MSB of size of author
-	mov	[idx16], Y, tmp_size ; size of author string
-	add16	idx16, #4	; move to start of author
-
-	;; copy the author string
-	strcpyp2c	idx16, author, tmp_size
-
-	add16	idx16, tmp_size	; move past the author string
-
-	ldy	#3		; MSB of guy x
-	mov	[idx16], Y, gx
-	add16	idx16, #4
-	ldy	#3		; MSB of guy y
-	mov	[idx16], Y, gy
-	add16	idx16, #4
-
-		
-	;; start rledecoding
- 	mov16	#tiles, tmp_addr
-	jsr	rledecode
-
-
-	debug_p	ds_tiles
-	mov	#tiles, debug_num
-	mov	(#tiles)+1, debug_num
-	ldx	#0
-.tile_print:	
-	mov	tiles, X, debug_num
-	inx
-	txa
-	cmp	#180
-	bne	.tile_print
-	
-	mov16	#otiles, tmp_addr
-  	jsr	rledecode
-	mov16	#dests, tmp_addr
- 	jsr	rledecode
-	mov16	#flags, tmp_addr
- 	jsr	rledecode
-
-	rts
-		
-	
-
-	.include "rle.asm"			
 		
 ;;; some data
 ;;; data
@@ -753,6 +271,7 @@ ds1:	.db	"rledecode",0
 ds_ppu:	.db	"ppu_on",0
 ds_run:	.db	"run",0
 ds_antirun:	.db	"arun",0
+ds_nmi:	.db	"nmi",0
 ds_x:	.db	"x",0
 ds_y:	.db	"y",0
 ds_i:	.db	"i",0
--- interrupts.asm
+++ interrupts.asm
@@ -2,4 +2,15 @@ intr:	sei
 	rti
 
 nmi:	
+;;; 	debug_p ds_nmi
+	lda	#0
+	sta	$2006
+	sta	$2006
+	lda	x_scroll
+	sta	$2005
+	lda	#0
+	sta	$2005
+
+	lda	#sprite/$100	; sprite
+	sta	$4014
 	rti
